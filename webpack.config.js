f63463ca5d77e0a51c576071d08eb2f5234a9078
DIYgod <i@diygod.me>
2021-10-30 00:58:43 +0100

    chore: remove webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const typescriptIsTransformer = require('typescript-is/lib/transform-inline/transformer').default;
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = {
     entry: {
@@ -50,7 +49,6 @@ const config = {
             util: require.resolve('util/'),
         },
     },
-    plugins: [new BundleAnalyzerPlugin()],
     devServer: {
         compress: true,
         static: './',
