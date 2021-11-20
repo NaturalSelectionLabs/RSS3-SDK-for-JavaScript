e7f24e4587bd2279d3720382a3b285334a2dd54a
DIYgod <i@diygod.me>
2021-11-20 09:16:15 +0000

    test: add test for check.removeCustomProperties

--- test/utils/check.ts
+++ test/utils/check.ts
@@ -15,3 +15,17 @@ test('check.valueLength', () => {
         }),
     ).toBe(false);
 });
+
+test('check.removeCustomProperties', () => {
+    const obj = {
+        test1: 'r',
+        test2: {
+            _test3: 'r',
+        },
+    };
+    check.removeCustomProperties(obj);
+    expect(obj).toMatchObject({
+        test1: 'r',
+        test2: {},
+    });
+});
