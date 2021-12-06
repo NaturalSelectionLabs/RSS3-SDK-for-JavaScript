ff6ee7acb27f2fec5667dcd9b2c13dff4373c7d2
DIYgod <i@diygod.me>
2021-12-06 15:07:51 +0000

    feat: add items getListByLink and assets getDetails

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,14 +1,32 @@
 import Main from '../index';
 import AutoAssets from './auto';
 import CustomAssets from './custom';
+import axois from 'axios';
+import type { AnyObject } from '../../types/extend';
 
 class Assets {
+    private main: Main;
+
     auto: AutoAssets;
     custom: CustomAssets;
 
     constructor(main: Main) {
+        this.main = main;
         this.auto = new AutoAssets(main);
         this.custom = new CustomAssets(main);
+    }
+
+    async getDetails(options: { persona: string; assets: string[]; full?: boolean }) {
+        const response = await axois({
+            method: 'get',
+            url: `${this.main.options.endpoint}/assets/details`,
+            params: {
+                persona: options.persona,
+                assets: options.assets.join(','),
+                full: options.full ? '1' : '0',
+            },
+        });
+        return <AnyObject[]>response.data.data;
     }
 }
 
--- src/items/index.ts
+++ src/items/index.ts
@@ -1,14 +1,32 @@
 import Main from '../index';
 import AutoItems from './auto';
 import CustomItems from './custom';
+import axois from 'axios';
 
 class Items {
+    private main: Main;
+
     auto: AutoItems;
     custom: CustomItems;
 
     constructor(main: Main) {
+        this.main = main;
         this.auto = new AutoItems(main);
         this.custom = new CustomItems(main);
+    }
+
+    async getListByLink(options: { limit: number; tsp: string; persona: string; linkID: string }) {
+        const response = await axois({
+            method: 'get',
+            url: `${this.main.options.endpoint}/items/list`,
+            params: {
+                limit: options.limit,
+                tsp: options.tsp,
+                persona: options.persona,
+                linkID: options.linkID,
+            },
+        });
+        return <(RSS3CustomItem | RSS3AutoItem)[]>response.data.data;
     }
 }
 
