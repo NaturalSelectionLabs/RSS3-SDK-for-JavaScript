93dca7b5f0bce80167cd7757a0461027c1f56d1b
DIYgod <i@diygod.me>
2021-12-13 14:17:43 +0000

    test: fix account sign with singer test case

--- test/account/index.ts
+++ test/account/index.ts
@@ -2,19 +2,19 @@ import RSS3 from '../../src/index';
 import { ethers } from 'ethers';
 import object from '../../src/utils/object';
 
-test('Account.sign with singer', () => {
+test('Account.sign with singer', async () => {
     const signer = ethers.Wallet.createRandom();
     const rss3 = new RSS3({
         endpoint: '',
         mnemonic: signer.mnemonic.phrase,
     });
-    expect(
-        rss3.account.sign({
-            agent_id: 'test',
-            test1: 'r',
-        }),
-    ).toEqual(
-        signer.signMessage(
+    const data: any = {
+        agent_id: 'test',
+        test1: 'r',
+    };
+    await rss3.account.sign(data);
+    expect(data.signature).toEqual(
+        await signer.signMessage(
             object.stringifyObj({
                 test1: 'r',
             }),
